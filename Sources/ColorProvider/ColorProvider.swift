335e11da7e83eb306f817a501c65f512228ee6d8
clydehsieh <clydehsieh0601@gmail.com>
2023-01-03 15:18:57 +0800

    Add red

--- Sources/ColorProvider/ColorProvider.swift
+++ Sources/ColorProvider/ColorProvider.swift
@@ -1,6 +1,13 @@
+import UIKit
+
 public struct ColorProvider {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    /// default
+    static func red() -> UIColor {
+        .red
     }
 }
