ddbe71dd326fc7ea401fe6718e90f8f2fe396974
clydehsieh <clydehsieh0601@gmail.com>
2023-01-03 15:23:47 +0800

    Add gray

--- Sources/ColorProvider/ColorProvider.swift
+++ Sources/ColorProvider/ColorProvider.swift
@@ -6,7 +6,7 @@ public struct ColorProvider {
     public init() {
     }
     
-    /// default
+    /// before 1.0.0
     static func red() -> UIColor {
         .red
     }
@@ -19,5 +19,10 @@ public struct ColorProvider {
     /// ver 2.0.0
     static func yellow() -> UIColor {
         .yellow
+    }
+    
+    /// after 2.0.0
+    static func gray()  -> UIColor {
+        .gray
     }
 }
